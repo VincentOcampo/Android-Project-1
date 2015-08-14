b5de490f2bc4409d1370c31bb0f62df8eaff7506
Vicente Ocampo <vinceocam@gmail.com>
2015-08-13 21:59:22 -0700

    Created a simple layout and implemented adapter to bind image to Gridview

--- AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/MainActivity.java
+++ AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/MainActivity.java
@@ -4,15 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+//Vicente Ocampo
+//Udacity Project
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // xml contains fragment that contains grid
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/MainActivityFragment.java
+++ AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/MainActivityFragment.java
@@ -1,10 +1,16 @@
 package com.example.vicenteocampo.andpractice;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,10 +19,65 @@ public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
     }
-
+    // Create view for fragment and binds data from Picasso to gridview
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridview = (GridView) rootView.findViewById(R.id.grid);
+
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        return rootView;
+    }
+
+    //extends BaseAdapter to be able images to Gridview
+    public class ImageAdapter extends BaseAdapter{
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        // Have Picasso handle image processing
+        public View getView(int position, View convertView, ViewGroup parent) {
+             ImageView view = (ImageView) convertView;
+             if (view == null) {
+                view = new ImageView(mContext);
+                 view.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                         ViewGroup.LayoutParams.MATCH_PARENT));
+                 view.setScaleType(ImageView.ScaleType.FIT_XY);
+                 view.setPadding(1, 1, 1, 1);
+             }else{
+                 view = (ImageView) convertView;
+             }
+
+
+            Picasso.with(mContext).load(mThumbIds[position]).into(view);
+
+            return view;
+        }
+
+        // references to our images
+        private String[] mThumbIds = {
+                "http://i.imgur.com/yRYXonm.jpg",
+                "http://i.imgur.com/yRYXonm.jpg",
+                "http://i.imgur.com/yRYXonm.jpg",
+                "http://i.imgur.com/yRYXonm.jpg"
+        };
     }
 }
