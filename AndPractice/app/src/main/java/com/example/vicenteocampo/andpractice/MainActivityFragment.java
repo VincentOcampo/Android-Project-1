2ef170e330e8211bb863c52b9618eaea60b41b0f
Vicente Ocampo <vinceocam@gmail.com>
2015-08-15 21:37:12 -0700

    Added functioning ImageAdapter and use of TMDb API

--- AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/MainActivityFragment.java
+++ AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/MainActivityFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +14,29 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    ImageAdapter gridAdapter;
 
     public MainActivityFragment() {
+
     }
-    // Create view for fragment and binds data from Picasso to gridview
+
+    // Inflate our layout and attach adapter to GridView
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,40 +44,111 @@ public class MainActivityFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         GridView gridview = (GridView) rootView.findViewById(R.id.grid);
         //Adapter places images into the Gridview object
-        gridview.setAdapter(new ImageAdapter(getActivity()));
-        //TODO
-        new fetchMovieImages().execute("dsdsd");
+        gridAdapter = new ImageAdapter(getActivity());
+        gridview.setAdapter(gridAdapter);
+
+        //Query for data, API key is omitted on public repo
+        // Source: https://www.themoviedb.org/documentation/api?language=en
+        new fetchMovieImages().execute("http://api.themoviedb.org/3/discover/movie?sort_" +
+                "by=popularity.desc&api_key=[YOUR API KEY]");
         return rootView;
     }
+    //TODO onclickupdate
+    // Parses data from query and updates View with adapter in a background thread
+    public class fetchMovieImages extends AsyncTask<String, Void, String[]> {
+        //JSON Parsing
+        public String[] getSortedMoviesArray(String result) throws JSONException {
+            String baseString = "http://image.tmdb.org/t/p/w500/";
+            String[] finalList;
+            JSONObject reader = new JSONObject(result);
 
-    public class fetchMovieImages extends AsyncTask<String,Void,String>{
+            JSONArray topRatedMovies = reader.getJSONArray("results");
+
+            finalList = new String[topRatedMovies.length()];
+            Integer j = topRatedMovies.length();
+
+            for (int i = 0; i < topRatedMovies.length(); i++) {
+                JSONObject movie = topRatedMovies.getJSONObject(i);
+                finalList[i] = baseString + movie.getString("poster_path");
+            }
+
+            return finalList;
+        }
 
         @Override
-        protected String doInBackground(String... params) {
+        protected String[] doInBackground(String... params) {
+            String result;
+            InputStream incoming = null;
+            try {
+
+                URL url = new URL(params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+
+                incoming = connection.getInputStream();
+                InputStreamReader is = new InputStreamReader(incoming);
+                BufferedReader reader = new BufferedReader(is);
+                StringBuilder buildData = new StringBuilder();
+
+                if (incoming == null) {
+                    return null;
+                } else {
+
+                    while ((result = reader.readLine()) != null) {
+                        buildData.append(result + "/n");
+                    }
+                    result = buildData.toString();
 
 
+                }
 
+            } catch (IOException e) {
+                return null;
+
+            } finally {
+                if (incoming != null) {
+                    try {
+                        incoming.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+            try {
+                // send result to be parsed
+                return getSortedMoviesArray(result);
+            } catch (JSONException e) {
+                Log.e("Sort", e.getMessage());
+            }
 
             return null;
         }
 
-        protected void onPostExecute(String[] result) {}
+        protected void onPostExecute(String[] result) {
+
+            for (String movie : result) {
+                gridAdapter.add(movie);
+            }
+            //Update our View
+            gridAdapter.notifyDataSetChanged();
+
+        }
     }
+
     //extends BaseAdapter to be able add images to Gridview
 
-
-
-
-
-    public class ImageAdapter extends BaseAdapter{
+    //Populates our gridview with images
+    public class ImageAdapter extends BaseAdapter {
         private Context mContext;
+        private ArrayList<String> mThumbIds;
 
         public ImageAdapter(Context c) {
             mContext = c;
+            mThumbIds = new ArrayList<String>();
         }
 
         public int getCount() {
-            return mThumbIds.length;
+            return mThumbIds.size();
         }
 
         public Object getItem(int position) {
@@ -75,29 +162,29 @@ public class MainActivityFragment extends Fragment {
         // create a new ImageView for each item referenced by the Adapter
         // Have Picasso handle image processing
         public View getView(int position, View convertView, ViewGroup parent) {
-             ImageView view = (ImageView) convertView;
-             if (view == null) {
+            ImageView view = (ImageView) convertView;
+            if (view == null) {
                 view = new ImageView(mContext);
-                 view.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                         ViewGroup.LayoutParams.MATCH_PARENT));
-                 view.setScaleType(ImageView.ScaleType.FIT_XY);
-                 view.setPadding(1, 1, 1, 1);
-             }else{
-                 view = (ImageView) convertView;
-             }
+                view.setAdjustViewBounds(true);
+                view.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+                view.setScaleType(ImageView.ScaleType.FIT_XY);
+                view.setPadding(5, 5, 5, 5);
+            } else {
+                view = (ImageView) convertView;
+            }
 
 
-            Picasso.with(mContext).load(mThumbIds[position]).into(view);
+            Picasso.with(mContext).load(mThumbIds.get(position)).into(view);
 
             return view;
         }
 
-        // references to our images
-        private String[] mThumbIds = {
-                "http://i.imgur.com/yRYXonm.jpg",
-                "http://i.imgur.com/yRYXonm.jpg",
-                "http://i.imgur.com/yRYXonm.jpg",
-                "http://i.imgur.com/yRYXonm.jpg"
-        };
+        public void add(String result) {
+            Log.v("imgAdd", result);
+            mThumbIds.add(result);
+        }
+
+
     }
 }
