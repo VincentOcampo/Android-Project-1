d760d23188494caa18660cb9944e8c358ebce5b6
Vicente Ocampo <vinceocam@gmail.com>
2015-08-27 21:27:01 -0700

    updated sqlite contract

--- AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/data/movieContract.java
+++ AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/data/movieContract.java
@@ -1,5 +1,6 @@
 package com.example.vicenteocampo.andpractice.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,6 +10,10 @@ public class movieContract {
 
     // defined columns of database for a movie entry
     public static final class MovieEntry implements BaseColumns {
+
+        public static final String CONTENT_AUTHORITY = "com.example.vicenteocampo.and";
+
+        public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
         public static final String TABLE_NAME = "movieDB";
 
--- AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/data/movieDB.java
+++ AndPractice/app/src/main/java/com/example/vicenteocampo/andpractice/data/movieDB.java
@@ -15,15 +15,27 @@ public class movieDB extends SQLiteOpenHelper {
 
 
      movieDB(Context context){
+
          super(context,MovieEntry.TABLE_NAME,null, DATABASE_VERSION);
      }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " );
+        db.execSQL("CREATE TABLE " + MovieEntry.TABLE_NAME + " (" +
+                MovieEntry.COLUMN_ID + " INTEGER PRIMARY KEY, " +
+                MovieEntry.COLUMN_POSTER + " TEXT UNIQUE NOT NULL, " +
+                MovieEntry.COLUMN_RATING + " TEXT UNIQUE NOT NULL, " +
+                MovieEntry.COLUMN_RELEASE + " TEXT UNIQUE NOT NULL, " +
+                MovieEntry.COLUMN_REVIEW + " TEXT UNIQUE NOT NULL, " +
+                MovieEntry.COlUMN_SUMMARY + " TEXT UNIQUE NOT NULL, " +
+                MovieEntry.COLUMN_TRAILER + " TEXT UNIQUE NOT NULL, " + " );");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        db.execSQL("DROP TABLE IF EXISTS " + MovieEntry.TABLE_NAME);
+        onCreate(db);
 
     }
 }
